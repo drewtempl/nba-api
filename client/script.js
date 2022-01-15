c7281ce301c68bd5c15fe2e53bf42eea876ab7c3
Drew Templeton <templetonag@appstate.edu>
2022-01-15 18:30:22 -0500

    roster display

--- client/script.js
+++ client/script.js
@@ -1,17 +1,11 @@
 const container = document.querySelector('.container');
+const header = document.querySelector('h1');
 
 async function getTeams() {
     const response = await fetch('http://localhost:3000/api/teams', {mode: 'cors'});
     
     console.log(response)
     const teamsData = await response.json();
-    
-
-
-    // const teamsData = await response.json();
-    //console.log(teamsData)
-
-    
 
     for (let i = 0; i < teamsData.length; i++) {
         
@@ -42,6 +36,20 @@ async function requestTeam(teamAbbrev) {
     console.log(response)
     const playersData = await response.json();
     console.log(playersData)
+
+    container.style.display = "none";
+
+    const roster = document.querySelector('.roster');
+
+    for (let i = 0; i < playersData.length; i++) {
+        
+        let card = document.createElement('div');
+        card.textContent = `${playersData[i].first_name} ${playersData[i].last_name}`;
+        card.classList.toggle('nba-player');
+        roster.appendChild(card)
+    }
+
+    header.textContent = `${playersData[0].team}`.toUpperCase();
 }
 
 getTeams();
